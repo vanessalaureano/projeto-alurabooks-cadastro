a5db81eccddeae050fe143ad1b6b5d741f853de8
vanessalaureano <solytagamer@gmail.com>
2023-07-09 21:02:51 -0300

    update

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`) // await = aguardam
         var consultaCEPConvertida = await consultaCEP.json()
         if(consultaCEPConvertida.erro) {
-            throw Error('Esse CEP não existe.');
+            throw Error('Esse CEP não existe.'); // throw = lançar
         }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
+
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch (erro) {
+        mensagemErro.innerHTML = `<p>O CEP é inválido. Tente novamente.</p>`;
         console.log(erro)
     }
 }
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
 
 /* let ceps = ['01001000', '01001001', '01001005'];
 let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
